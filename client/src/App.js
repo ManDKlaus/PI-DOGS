4408989a38d072d5281e9ab643b176e73b822093
Man.D.Klaus <mansilland@gmail.com>
2023-05-01 14:41:50 -0300

    Terminado: Componente Welcome. Pendientes: en Componente App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route, useLocation} from "react-router-dom";
 import Nav from "./components/Nav/Nav.jsx";
 import Welcome from "./components/Welcome/Welcome.jsx";
@@ -8,6 +8,7 @@ import Home from "./components/Home/Home.jsx";
 import Dogs from './components/Dogs/Dogs.jsx';
 import Temperaments from "./components/Temperaments/Temperaments.jsx";
 import About from './components/About/About.jsx' ;
+import Details from "./components/Details/Details.jsx"
 import Footer from "./components/Footer/Footer.jsx";
 import { searchTemps, searchDogs } from "./redux/actions/actions.js"
 
@@ -15,45 +16,55 @@ function App() {
 
    const location = useLocation();
    const dispatch = useDispatch();
-   const { allFavs, allDogs } = useSelector((s) => s);
 
    useEffect(() => {
       dispatch(searchTemps());
-   }, []);
-   
-   useEffect(() => {
       dispatch(searchDogs());
-   }, [allFavs, allDogs]);
+   }, []);
 
-  return (
-    <div className="App">
-      {
-         location.pathname === "/" ? null : <Nav />
-      }
-      <Routes>
-         <Route
-            path="/"
-            element={<Welcome />}
-         />
-         <Route 
-            path="/home"
-            element={<Home />}
-         />
-         <Route 
-            path="/about"
-            element={<About />}
-         />
-         <Route 
-            path="/dogs"
-            element={<Dogs />}
-         />
-         <Route
-            path="/temperaments"
-            element={<Temperaments />}
-         />
-      </Routes>
-      <Footer />
-    </div>
+   const [mode, setMode] = useState('day');
+   function handlerMode() {
+      setMode(mode === 'day' ? 'night' : 'day');
+   };
+
+   return (
+      <div className={`App ${mode}`}>
+         <div className='Mode' >
+            <button onClick={handlerMode} />
+         </div>
+         {
+            location.pathname === "/" ? null : <Nav />
+         }
+         <Routes>
+            <Route
+               path="/"
+               element={<Welcome />}
+            />
+            <Route 
+               path="/home"
+               element={<Home />}
+            />
+            <Route 
+               path="/about"
+               element={<About />}
+            />
+            <Route 
+               path="/dogs"
+               element={<Dogs />}
+            />
+            <Route
+               path="/temperaments"
+               element={<Temperaments />}
+            />
+            <Route 
+               path="/details"
+               element={<Details />}
+            />
+         </Routes>
+         {
+            location.pathname === "/" ? null : <Footer />
+         }
+      </div>
   );
 }
 
--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -1,12 +1,62 @@
 import "./Welcome.css";
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Welcome() {
-  return (
-    <div className='Welcome'>
-        <h1>Welcome to my dogs page</h1>
-        <Link to="/home">Start adventure</Link>
-    </div>
-  )
+    const [buttonsPressed, setButtonsPressed] = useState(false);
+
+    const handleButtonPress = () => {
+        setButtonsPressed(true);
+      };
+      
+      useEffect(() => {
+        const welcome = document.querySelector(".App");
+        const blocks = Array.from({ length: 160 }, () => {
+          const size = getRandomInt(20, 60);
+          const block = document.createElement("div");
+          block.classList.add("block");
+          block.style.transform = `scale(${Math.random() * (2 - 1) + 1})`;
+          block.style.top = `${getRandomInt(0, window.innerHeight)}px`;
+          block.style.left = `${getRandomInt(0, window.innerWidth)}px`;
+          block.style.transform = `rotate(${getRandomInt(0, 360)}deg)`;
+          block.style.width = `${size}px`;
+          block.style.height = `${size}px`;
+          welcome.appendChild(block);
+          return block;
+        });
+        
+        return () => {
+          blocks.forEach((block) => welcome.removeChild(block));
+        }
+      }, []);
+      
+      useEffect(() => {
+        if (buttonsPressed) {
+          const blocks = document.querySelectorAll(".block");
+          blocks.forEach((block) => {
+            const size = getRandomInt(20, 60);
+            block.style.transform = `scale(${Math.random() * (2 - 1) + 1})`;
+            block.style.top = `${getRandomInt(0, window.innerHeight)}px`;
+            block.style.left = `${getRandomInt(0, window.innerWidth)}px`;
+            block.style.transform = `rotate(${getRandomInt(0, 360)}deg)`;
+            block.style.width = `${size}px`;
+            block.style.height = `${size}px`;
+          });
+        }
+        setButtonsPressed(false);
+      }, [buttonsPressed]);
+      
+      function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+
+    return (
+        <div className='Welcome'>
+            <h1>Welcome to my dogs page</h1>
+            <button onClick={handleButtonPress}>Do you like dogs?</button>
+            <button onClick={handleButtonPress}>Do you want to learn more about them?</button>
+            <Link to="/home" id="wel" >Start adventure</Link>
+        </div>
+    )
 }
